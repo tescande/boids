0f95a45bc75abfea4ec65c42b0bf34652b08ffdf
Thierry Escande <tescande@gmail.com>
2021-04-10 18:10:35 +0200

    Fix a typo in Greenish

--- gui.c
+++ gui.c
@@ -681,7 +681,7 @@ static void gui_show(BoidsGui *gui)
 	combo = gtk_combo_box_text_new();
 	gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), BG_COLOR_WHITE, NULL, "White");
 	gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), BG_COLOR_REDDISH, NULL, "Reddish");
-	gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), BG_COLOR_GREENISH, NULL, "Greenisg");
+	gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), BG_COLOR_GREENISH, NULL, "Greenish");
 	gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), BG_COLOR_BLUISH, NULL, "Bluish");
 	gtk_combo_box_set_active(GTK_COMBO_BOX(combo), swarm_get_bg_color(gui->swarm));
 	g_signal_connect(G_OBJECT(combo), "changed",
